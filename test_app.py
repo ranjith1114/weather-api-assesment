2fe9e0753c38a352f7510679734280855c84ff68
Ranjith Reddy Mada <ranjithreddymada@Ranjiths-MacBook-Pro.local>
2023-04-21 14:57:20 -0500

    updating the files accordingly

--- test_app.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from app import app
-
-def test_get_weather():
-    with app.test_client() as client:
-        response = client.get('/api')
-        assert response.status_code == 200
